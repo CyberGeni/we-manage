78e9efd53d1559a409aa175d77b9ce40cc561fde
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-11-22 23:57:59 +0100

    project setup for vue and tailwind

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1 class="text-center text-2xl">Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/assets/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1 class="">Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
